2a32ef8aa60b01cb27824838e637c6d0ad73d184
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2022-01-23 08:08:44 +0300

    infinite query added

--- React/e-commerce-app/client/src/api.js
+++ React/e-commerce-app/client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const fetchProductList = async () => {
-  const { data } = await axios.get(`${process.env.REACT_APP_BASE_ENDPOINT}/product`);
+export const fetchProductList = async ({ pageParam = 0}) => {
+  const { data } = await axios.get(`${process.env.REACT_APP_BASE_ENDPOINT}/product?page=${pageParam}`);
   return data;
 };
 
--- React/e-commerce-app/client/src/pages/Products/index.js
+++ React/e-commerce-app/client/src/pages/Products/index.js
@@ -1,25 +1,61 @@
-import { Grid } from "@chakra-ui/react";
-import { useQuery } from "react-query";
+import { Box, Flex, Grid, Button } from "@chakra-ui/react";
+import { useInfiniteQuery } from "react-query";
 
 import Card from "../../components/Card";
-import { fetchProductList} from '../../api'
+import { fetchProductList } from "../../api";
+import React from "react";
 
 export default function Products() {
-  const { isLoading, error, data } = useQuery("products", fetchProductList);
+  const {
+    data,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    status,
+  } = useInfiniteQuery("products", fetchProductList, {
+    getNextPageParam: (lastGroup, allGroup) => {
+      const morePageExist = lastGroup?.length === 12;
 
-  if (isLoading) return "Loading...";
+      if (!morePageExist) {
+        return;
+      }
 
-  if (error) return "An error has occurred: " + error.message;
+      return allGroup.length + 1;
+    },
+  });
+
+  if (status === "loading") return "Loading...";
+
+  if (status === "error") return "An error has occurred: " + error.message;
 
   return (
     <div>
       <Grid templateColumns="repeat(3, 1fr)" gap={4}>
-        {
-          data.map((item, key) => (
-            <Card key={key} item={item} />
-          ))
-        }
+        {data.pages.map((group, i) => (
+          <React.Fragment key={i}>
+            {group.map((item) => (
+              <Box width="100%" key={item._id}>
+                <Card item={item} />
+              </Box>
+            ))}
+          </React.Fragment>
+        ))}
       </Grid>
+
+      <Flex mt="10" justifyContent="center">
+        <Button
+          onClick={() => fetchNextPage()}
+          isLoading={isFetchingNextPage}
+          disabled={!hasNextPage || isFetchingNextPage}
+        >
+          {isFetchingNextPage
+            ? "Loading more..."
+            : hasNextPage
+            ? "Load More"
+            : "Nothing more to load"}
+        </Button>
+      </Flex>
     </div>
   );
 }
