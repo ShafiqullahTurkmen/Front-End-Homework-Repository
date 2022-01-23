0c812589ed5c87356d32c316645f8a964a47bd7c
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2022-01-23 10:07:30 +0300

    user registration

--- React/e-commerce-app/client/src/api.js
+++ React/e-commerce-app/client/src/api.js
@@ -1,11 +1,23 @@
 import axios from "axios";
 
-export const fetchProductList = async ({ pageParam = 0}) => {
-  const { data } = await axios.get(`${process.env.REACT_APP_BASE_ENDPOINT}/product?page=${pageParam}`);
+export const fetchProductList = async ({ pageParam = 0 }) => {
+  const { data } = await axios.get(
+    `${process.env.REACT_APP_BASE_ENDPOINT}/product?page=${pageParam}`
+  );
   return data;
 };
 
 export const fetchProduct = async (id) => {
-  const { data } = await axios.get(`${process.env.REACT_APP_BASE_ENDPOINT}/product/${id}`);
+  const { data } = await axios.get(
+    `${process.env.REACT_APP_BASE_ENDPOINT}/product/${id}`
+  );
+  return data;
+};
+
+export const fetchRegister = async (input) => {
+  const { data } = await axios.post(
+    `${process.env.REACT_APP_BASE_ENDPOINT}/auth/register`,
+    input
+  );
   return data;
 };
--- React/e-commerce-app/client/src/pages/Auth/Signup/index.jsx
+++ React/e-commerce-app/client/src/pages/Auth/Signup/index.jsx
@@ -7,9 +7,13 @@ import {
   FormLabel,
   Input,
   Button,
+  Alert
 } from "@chakra-ui/react";
+
 import { useFormik } from "formik";
 import validationSchema from "./validations";
+
+import { fetchRegister } from "../../../api";
 
 export default function Signup() {
   const formik = useFormik({
@@ -20,7 +24,15 @@ export default function Signup() {
     },
     validationSchema,
     onSubmit: async (values, bag) => {
-      console.log(values);
+      try {
+        const registerResponse = await fetchRegister({
+          email: values.email,
+          password: values.password,
+        });
+        console.log(registerResponse);
+      } catch (e) {
+        bag.setErrors({ general: e.response.data.message})
+      }
     },
   });
 
@@ -31,6 +43,15 @@ export default function Signup() {
           <Box textAlign="center">
             <Heading>SignUp</Heading>
           </Box>
+          <Box my={5}>
+            {
+              formik.errors.general && (
+                <Alert status="error">
+                  { formik.errors.general}
+                </Alert>
+              )
+            }
+          </Box>
           <Box my={5} textAlign="left">
             <form onSubmit={formik.handleSubmit}>
               <FormControl>
@@ -40,6 +61,7 @@ export default function Signup() {
                   onChange={formik.handleChange}
                   onBlur={formik.handleBlur}
                   value={formik.values.email}
+                  isInvalid={formik.touched.email && formik.errors.email}
                 />
               </FormControl>
 
@@ -51,6 +73,7 @@ export default function Signup() {
                   onChange={formik.handleChange}
                   onBlur={formik.handleBlur}
                   value={formik.values.password}
+                  isInvalid={formik.touched.password && formik.errors.password}
                 />
               </FormControl>
 
@@ -62,6 +85,10 @@ export default function Signup() {
                   onChange={formik.handleChange}
                   onBlur={formik.handleBlur}
                   value={formik.values.passwordConfirm}
+                  isInvalid={
+                    formik.touched.passwordConfirm &&
+                    formik.errors.passwordConfirm
+                  }
                 />
               </FormControl>
 
