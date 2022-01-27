847720dddc5717710e9d1a87145ca26847ec5cd1
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2022-01-27 08:40:36 +0300

    prepare added

--- Redux/to-do-app/src/components/Form.jsx
+++ Redux/to-do-app/src/components/Form.jsx
@@ -1,25 +1,24 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { addTodo } from "../redux/todos/todosSlice";
-import { nanoid } from "@reduxjs/toolkit";
 
 export default function Form() {
-  const [ title, setTitle ] = useState("");
+  const [title, setTitle] = useState("");
   const dispatch = useDispatch();
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
+    if (!title) return;
 
-    dispatch(addTodo({
-      id: nanoid(),
-      title,
-      completed: false
-    }));
+    dispatch(
+      addTodo({
+        title,
+      })
+    );
 
     setTitle("");
-
-  }
+  };
 
   return (
     <form onSubmit={handleSubmit}>
--- Redux/to-do-app/src/redux/todos/todosSlice.js
+++ Redux/to-do-app/src/redux/todos/todosSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 export const todosSlice = createSlice({
   name: "todos",
@@ -18,8 +18,19 @@ export const todosSlice = createSlice({
     activeFilter: "all",
   },
   reducers: {
-    addTodo: (state, action) => {
-      state.items = [...state.items, action.payload];
+    addTodo: {
+      reducer: (state, action) => {
+        state.items = [...state.items, action.payload];
+      },
+      prepare: ({ title }) => {
+        return {
+          payload: {
+            id: nanoid(),
+            title,
+            completed: false
+          }
+        }
+      }
     },
     toggle: (state, action) => {
       const { id } = action.payload;
